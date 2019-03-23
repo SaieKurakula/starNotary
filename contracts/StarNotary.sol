aea0e7737b102eb8796c207d09a158e58db9ae11
Saie Kurakula <saiekurakula@gmail.com>
2019-03-23 19:10:24 -0400

    Updating StarNotary

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -12,21 +12,23 @@ contract StarNotary is ERC721 {
     }
 
     // Implement Task 1 Add a name and symbol properties
-    // name: Is a short name to your token
-    // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string public constant name = "Saie Kurakula Token";
+    string public constant symbol = "SKT"; 
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
+    uint256[] public SDK_Tokens;
+
     
     // Create Star using the Struct
     function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
         Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
         tokenIdToStarInfo[_tokenId] = newStar; // Creating in memory the Star -> tokenId mapping
-        _mint(msg.sender, _tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
+        SDK_Tokens.push(_tokenId);
+	_mint(msg.sender, _tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
     }
 
     // Putting an Star for sale (Adding the star tokenid into the mapping starsForSale, first verify that the sender is the owner)
@@ -54,9 +56,20 @@ contract StarNotary is ERC721 {
         }
     }
 
+    // function to check if a star exists
+    function starExists (uint256 _tokenId) public view returns (bool){
+      for (uint i; i< SDK_Tokens.length;i++){
+          if (SDK_Tokens[i] == _tokenId )
+          return true;
+      }
+    }
+
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
+        require(starExists(_tokenId), "Please Send a Valid Star.");
+        Star memory star = tokenIdToStarInfo[_tokenId];
+        return star.name;
     }
 
     // Implement Task 1 Exchange Stars function
@@ -65,12 +78,17 @@ contract StarNotary is ERC721 {
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
+        require(ownerOf(_tokenId1) == msg.sender);
+        _transferFrom(ownerOf(_tokenId1),ownerOf(_tokenId2), _tokenId1);
+        _transferFrom(ownerOf(_tokenId2),ownerOf(_tokenId1), _tokenId2);
     }
 
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+	require(ownerOf(_tokenId) == msg.sender);
+	_transferFrom(msg.sender, _to1, _tokenId);
     }
 
 }
